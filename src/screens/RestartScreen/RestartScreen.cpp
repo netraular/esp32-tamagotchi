c87dd571a2584425ac53fda0d39bd8d05d5bfad1
netraular <netraular@gmail.com>
2025-01-09 16:57:43 +0100

    Intercambiadas funcionalidad restart y setup

--- src/screens/RestartScreen/RestartScreen.cpp
+++ src/screens/RestartScreen/RestartScreen.cpp
@@ -1,12 +1,15 @@
 #include "RestartScreen.h"
 #include "../ScreenManager.h"
 
+#include "../../PersistentDataManager/PersistentDataManager.h"
+
 extern ScreenManager screenManager;
+extern PersistentDataManager persistentDataManager;
 
 void RestartScreen::load() {
-    // Crear una etiqueta con el texto "delete files"
+    // Crear una etiqueta con el texto "start kodama"
     label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, "delete files");
+    lv_label_set_text(label, "start kodama");
     lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0); // Centrar el texto
     lv_obj_set_style_text_font(label, &lv_font_montserrat_16, 0); // Fuente mediana
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0); // Centrar en la pantalla
@@ -22,5 +25,14 @@ void RestartScreen::handleButtonEvent(const ButtonState& state, const ButtonChan
     // Si se presiona el botón 3, volver a la pantalla PetScreen
     if (change.button3Changed && state.button3Pressed) {
         screenManager.setScreen("PetScreen");
+    }
+
+    // Si se presiona el botón 1, resetear los archivos JSON
+    if (change.button1Changed && state.button1Pressed) {
+        if (persistentDataManager.resetFoodData() && persistentDataManager.resetOwnFoodData() && persistentDataManager.resetPetStats()) {
+            Serial.println("Archivos JSON reseteados correctamente");
+        } else {
+            Serial.println("Error al resetear los archivos JSON");
+        }
     }
 }
--- src/screens/SetupScreen/SetupScreen.cpp
+++ src/screens/SetupScreen/SetupScreen.cpp
@@ -1,14 +1,13 @@
 #include "SetupScreen.h"
 #include "../ScreenManager.h"
-#include "../../PersistentDataManager/PersistentDataManager.h"
 
 extern ScreenManager screenManager;
-extern PersistentDataManager persistentDataManager;
 
 void SetupScreen::load() {
-    // Crear una etiqueta con el texto "start kodama"
+
+    // Crear una etiqueta con el texto "delete files"
     label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, "start kodama");
+    lv_label_set_text(label, "delete files");
     lv_obj_set_style_text_align(label, LV_TEXT_ALIGN_CENTER, 0); // Centrar el texto
     lv_obj_set_style_text_font(label, &lv_font_montserrat_16, 0); // Fuente mediana
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0); // Centrar en la pantalla
@@ -25,13 +24,6 @@ void SetupScreen::handleButtonEvent(const ButtonState& state, const ButtonChange
     if (change.button3Changed && state.button3Pressed) {
         screenManager.setScreen("PetScreen");
     }
-
-    // Si se presiona el botón 1, resetear los archivos JSON
-    if (change.button1Changed && state.button1Pressed) {
-        if (persistentDataManager.resetFoodData() && persistentDataManager.resetOwnFoodData() && persistentDataManager.resetPetStats()) {
-            Serial.println("Archivos JSON reseteados correctamente");
-        } else {
-            Serial.println("Error al resetear los archivos JSON");
-        }
-    }
 }
+
+
