349df76cccba6416e4948551f335db90eba0e0f8
netraular <netraular@gmail.com>
2025-02-04 02:28:01 +0100

    Añadido pines para esp32 s3

--- src/config.h
+++ src/config.h
@@ -2,11 +2,12 @@
 #define CONFIG_H
 
 // Definición de pines
-#define BUZZER_PIN 32
-#define BUTTON1_PIN 27
-#define BUTTON2_PIN 26
-#define BUTTON3_PIN 25
-#define BUTTON4_PIN 33
+#define BUZZER_PIN 17
+#define BUTTON1_PIN 7
+#define BUTTON2_PIN 6
+#define BUTTON3_PIN 5
+#define BUTTON4_PIN 4
+// #define RGB_BUILTIN 48
 
 // Tiempo de debounce para los botones (en milisegundos)
 const unsigned long DEBOUNCE_DELAY = 50;
